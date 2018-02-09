3a6d6380bd2c458cc169f80fdd2e6c65f7fefba7
vmariia <vashchuk.mariia@gmail.com>
2018-02-09 22:23:51 +0200

    homework_06
    homework_06

--- homework_06/js/task1.js
+++ homework_06/js/task1.js
@@ -1,37 +1,39 @@
-var a = +prompt("First length of sides", 0);
-var b = +prompt("Second length of sides", 0);
-var c = +prompt("Third length of sides", 0);
+var a = parseFloat(prompt("First length of sides", 0));
+var b = parseFloat(prompt("Second length of sides", 0));
+var c = parseFloat(prompt("Third length of sides", 0));
 var triangleType;
 
-if (Number.isFinite(a) && Number.isFinite(b) && Number.isFinite(c)) {
-	if (a <= 0 || b <= 0 || c <= 0) {
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+	console.log("Not a number");
 	console.log("Incorrect data");
-	} else {	
-		if (a*a + b*b == c*c || b*b + c*c == a*a || c*c + a*a == b*b) {
-			triangleType = "Right triangle";
-		} else if (a === b && b === c && c === a) {
-			triangleType = "Equilateral";
-		} else if (a === b || b === c || c === a) {
-			triangleType = "Isosceles";
-		} else if (a !== b && b !== c && c !== a) {
-			triangleType = "Scalene";
-		}
-
-		var p = (a + b + c)/2;
-		console.log("p =", p);
-
-		if (p > a && p > b && p > c) {
-			var s = Math.sqrt(p*(p-a)*(p-b)*(p-c)).toFixed(2);
-			console.log("s =", s);
-			if (s == 0) {
-				console.log("Incorrect data");
-			} else {
-			   console.log("Type of triangle is " + triangleType + " and square is " + s);
-			}
-		} else {
-			console.log("p <= than a or b or c");
-		}
-	}
 } else {
-	console.log("Text was typed");
+    if (a < b + c && b < a + c && c < a + b) {
+     
+   		triangleType(); 
+
+  		var p = (a + b + c)/2;
+
+	 	var s = (Math.round(Math.sqrt(p*(p-a)*(p-b)*(p-c))*100)/100);
+
+	 	if (s == 0) {
+			console.log("Incorrect data");
+		} else {
+		   console.log("Type of triangle is " + triangleType + " and square is " + s);
+		}
+    } else {
+  		console.log("Triangle does not exist");
+  		console.log("Incorrect data");
+    }
+}
+
+function triangleType() {
+	if (a*a + b*b == c*c || b*b + c*c == a*a || c*c + a*a == b*b) {
+		triangleType = "Right triangle";
+	} else if (a === b && b === c && c === a) {
+		triangleType = "Equilateral";
+	} else if (a === b || b === c || c === a) {
+		triangleType = "Isosceles";
+	} else if (a !== b && b !== c && c !== a) {
+		triangleType = "Scalene";
+	}
 }
--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -1,21 +1,23 @@
-var euro = +prompt("EURO", 0);
-var usd = +prompt("USD", 0);
+var euro = parseFloat(prompt("EURO", 0));
+var usd = parseFloat(prompt("USD", 0));
 
 var courseEuro = 33.8565;
 var courseUsd = 27.4609;
 
 
-if (Number.isFinite(euro) && Number.isFinite(usd)) {
+if (isNaN(euro) || isNaN(usd)) {
+	console.log("Not a number");
+	console.log("Incorrect data");	
+} else {
 	if (euro > 0 && usd > 0) {
+
 		var e = euro * courseEuro;
 		var u = usd * courseUsd;
 		var oneEuro = courseEuro / courseUsd;
-		console.log(euro + " euros are equal " + e.toFixed(3) + " UAH, "
-		+ usd + " dollars are equal " + u.toFixed(3) + 
-		" UAH, one euro is equal " + oneEuro.toFixed(3) + " dollars.");
+		console.log(euro + " euros are equal " + e.toFixed(2) + " UAH, "
+		+ usd + " dollars are equal " + u.toFixed(2) + 
+		" UAH, one euro is equal " + oneEuro.toFixed(2) + " dollars.");
 	} else {
-		console.log("euro or usd < or = 0");
+		console.log("Incorrect data");
 	}
-} else {
-	console.log("Text was typed");
 }
