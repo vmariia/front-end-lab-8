e7076f2f8b90e84e066f442d3a017a39e87f26af
vmariia <vashchuk.mariia@gmail.com>
2018-02-11 20:19:56 +0200

    homework_06
    homework_06

--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -1,8 +1,8 @@
 var euro = parseFloat(prompt("EURO", 0));
 var usd = parseFloat(prompt("USD", 0));
 
-var courseEuro = 33.8565;
-var courseUsd = 27.4609;
+var courseEuro = 33.2324;
+var courseUsd = 27.1240;
 
 
 if (isNaN(euro) || isNaN(usd)) {
@@ -10,13 +10,12 @@ if (isNaN(euro) || isNaN(usd)) {
 	console.log("Incorrect data");	
 } else {
 	if (euro > 0 && usd > 0) {
-
 		var e = euro * courseEuro;
 		var u = usd * courseUsd;
 		var oneEuro = courseEuro / courseUsd;
-		console.log(euro + " euros are equal " + e.toFixed(2) + " UAH, "
-		+ usd + " dollars are equal " + u.toFixed(2) + 
-		" UAH, one euro is equal " + oneEuro.toFixed(2) + " dollars.");
+		console.log(euro + " euros are equal " + Math.round(e*100)/100 + " UAH, "
+		+ usd + " dollars are equal " + Math.round(u*100)/100 + 
+		" UAH, one euro is equal " + Math.round(oneEuro*100)/100 + " dollars.");
 	} else {
 		console.log("Incorrect data");
 	}
