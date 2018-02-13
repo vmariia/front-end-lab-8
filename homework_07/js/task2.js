284eb40fa9f0970b6e30d9827dc8836e7966ee80
vmariia <vashchuk.mariia@gmail.com>
2018-02-13 20:58:40 +0200

    homework_07
    homework_07

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,60 +1,52 @@
 var maxNumber = 5;
 var firstPrize = 10;
-var secondPrize = 5;
-var thirdPrize = 2;
 var totalPrize = 0;
-var totalAtempts = 3;
+var totalAttempts = 3;
 var continueGame = false;
-var leftAtempts;
 var message = confirm("Do you want to play a game?");
-
-if (!message) {   
-	console.log("You did not become a millionaire");
+if (!message) {
+    console.log("You did not become a millionaire");
 } else {
-	while (true) {
-		var winNumber = Math.floor(Math.random() * maxNumber) + 1;
-		console.log(winNumber);
-		for (var i = 0; i < totalAtempts; i++)
-		{
-			var prize;
-			switch (i)
-			{
-			case 0:
-			    prize = firstPrize;
-			    break;
-			case 1:
-			    prize = secondPrize;
-			    break;
-			case 2:
-			    prize = thirdPrize;
-			    break;
-			default:
-			    prize = 0;
-			    break;
-			}
-			leftAtempts = totalAtempts - i; 
-			var userNumber = +prompt("Enter a number from 0 to " + maxNumber + 
-				        "\nAttempts left " + leftAtempts +
+    while (message) {
+        var winNumber = Math.floor(Math.random() * (maxNumber + 1));
+        console.log(winNumber);
+        for (var i = 0; i < totalAttempts; i++) {
+            var prize;
+            switch (i) {
+                case 0:
+                    prize = firstPrize;
+                    break;
+                case 1:
+                    prize = Math.floor(firstPrize / 2);
+                    break;
+                case 2:
+                    prize = Math.floor(firstPrize / 4);
+                    break;
+                default:
+                    prize = 0;
+                    break;
+            }
+            var userNumber = prompt("Enter a number from 0 to " + maxNumber +
+				        "\nAttempts left " + (totalAttempts - i) +
 				        "\nTotal prize " + totalPrize + "$" +
 				        "\nPossible prize on current attempt " + prize + "$");
-			if (winNumber == userNumber) {
-				totalPrize += prize;
-				var continueGame = confirm("Do you want to continue a game?");
-				continueGame = true;
-				break;
-			}
-		}
-		if (continueGame) {
+            if (!isNaN(userNumber) && winNumber == userNumber) {
+                totalPrize += prize;
+                console.log("Thank you for a game. Your prize is:" + totalPrize + "$");
+                var continueGame = confirm("Do you want to continue a game?");
+                continueGame = true;
+                break;
+            }
+        }
+        if (continueGame) {
             maxNumber *= 2;
             firstPrize *= 3;
-            secondPrize *= 3;
-            thirdPrize *= 3;
             continueGame = false;
         } else {
             console.log("Thank you for a game. Your prize is:" + totalPrize + "$");
-            var continueGame = confirm("Do you want to continue a game?");
+            var continueGame = confirm("Do you want to play again?");
             if (!continueGame)
                 break;
         }
-	}
+    }
 }
