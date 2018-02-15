c8b4243a380a4d31a1ee04a007e2fdcb4453fffd
vmariia <vashchuk.mariia@gmail.com>
2018-02-16 00:06:10 +0200

    homework_08
    homework_08

--- homework_08/js/getClosestToZero.js
+++ homework_08/js/getClosestToZero.js
@@ -1,7 +1,7 @@
 function getClosestToZero () {
 	var min = Math.abs(arguments[0]);
     var closestToZero;
-	for (var i = 0; i < arguments.length; i++) {
+	for (var i = 1; i < arguments.length; i++) {
 		if (Math.abs(arguments[i]) < min) {
 			var min = Math.abs(arguments[i]);
 			closestToZero = arguments[i];
